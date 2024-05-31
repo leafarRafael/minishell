7f93d53e08f1701e70927e0304cb830fa41db08e
onze-e-meia <tffuente@gmail.com>
2024-05-31 19:49:33 -0300

    Export Builtin

--- src/builtins/cmd/export.c
+++ src/builtins/cmd/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: tforster <tfforster@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:57:45 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/05/28 12:35:56 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/05/31 19:33:18 by tforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,31 @@ void export(t_ast_n *cmd, t_mini *mini, t_ast *ast, t_var_exe *var)
 	int		i_color;
 
 	ft_manager_fd(cmd, mini, ast, var);
-	line = mini->m_lst_env->head;
-	i = 1;
-	i_color = 0;
-	while (i <= mini->m_lst_env->size)
+	if (cmd->m_lst->matrix->size == 1)
 	{
-		if (i_color > 3)
-			i_color = 0;
-		ft_putstr_fd(mini->color[i_color], STDOUT_FILENO);
-		ft_putlst_fd(line->lst, 1, STDOUT_FILENO);
-		ft_putstr_fd(RESET, STDOUT_FILENO);
-		i_color++;
-		line = line->next;
-		i++;
+		line = mini->m_lst_env->head;
+		i = 1;
+		i_color = 0;
+		while (i <= mini->m_lst_env->size)
+		{
+			if (i_color > 3)
+				i_color = 0;
+			ft_putstr_fd(mini->color[i_color], STDOUT_FILENO);
+			ft_putlst_fd(line->lst, 1, STDOUT_FILENO);
+			ft_putstr_fd(RESET, STDOUT_FILENO);
+			i_color++;
+			line = line->next;
+			i++;
+		}
+		if (cmd->m_lst->next->type == PIPE)
+		{
+			close (mini->fd_std[0]);
+			close (mini->fd_std[1]);
+			free_memory(mini, var, ast, 1);
+		}
 	}
-	if (cmd->m_lst->next->type == PIPE)
-	{
-		close (mini->fd_std[0]);
-		close (mini->fd_std[1]);
-		free_memory(mini, var, ast, 1);
-	}
+	else
+		ft_putstr_fd("INCLUIR EXPORT\n", 2);
 }
 
 static void ft_manager_fd(t_ast_n *cmd, t_mini *mini, t_ast *ast, t_var_exe *var)
