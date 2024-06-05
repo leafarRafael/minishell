d9d884f3863721a1f632a126a0a69c3140e7de18
onze-e-meia <tffuente@gmail.com>
2024-06-05 17:44:37 -0300

    Valid input

--- src/minishell.c
+++ src/minishell.c
@@ -6,12 +6,12 @@
 /*   By: tforster <tfforster@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 08:43:23 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/06/05 16:13:59 by tforster         ###   ########.fr       */
+/*   Updated: 2024/06/05 17:41:16 by tforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "th_syntax.h"
+// #include "th_syntax.h"
 
 static void	ft_execute_minishell(t_mini *mini);
 void		ft_scanner_env(t_mlst *mlst);
@@ -24,7 +24,6 @@ int	main(void)
 	t_mini	mini;
 	char	*name;
 	int		i;
-	t_stx	stx;
 	int		index;
 
 	mini.m_lst_env = ft_cmtrix_to_mtrx_lst(__environ);
@@ -44,21 +43,21 @@ int	main(void)
 		name = ft_get_program_name();
 		ft_putstr_fd(name, 2);
 		free(name);
-		mini.input = readline(GRN " \033[1m\nminishel \u279C : \033[0m" RST);
+		mini.input = readline("\n minishell$ : ");
 		if (!mini.input)
 			break ;
-		add_history(mini.input);
-		stx.error = th_parse_param(mini.input);
-		if (!stx.error)
+		mini.status = ft_input_is_valid(mini.input);
+		if (!mini.status)
 		{
 			ft_putstr_fd(mini.color[i], 2);
-			// if (!ft_input_is_valid(mini.input))
 				ft_execute_minishell(&mini);
 			i++;
 			if (i == 6)
 				i = 1;
 			ft_putstr_fd(RESET, 2);
 		}
+		else
+			free(mini.input);
 		index = 0;
 		while (mini.ast[index] && index < 40)
 		{
