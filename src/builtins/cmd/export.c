5a5b850c8f209d59debd64aab19dee2b037775e5
onze-e-meia <tffuente@gmail.com>
2024-06-05 19:59:39 -0300

    Norm Export

--- src/builtins/cmd/export.c
+++ src/builtins/cmd/export.c
@@ -3,62 +3,104 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbutzke <rbutzke@student.42so.org.br>      +#+  +:+       +#+        */
+/*   By: tforster <tfforster@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:57:45 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/06/05 17:19:26 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/06/05 19:13:13 by tforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
 
-/* static void ft_manager_fd(t_ast_n *cmd, t_mini *mini, t_var_exe *var); */
-
-int	ft_cmpvar(t_lst *lst, char *str, int size)
-{
-	char	*new;
-	int		i;
-
-	i = 0;
-	new = ft_cpy_lst_to_array(lst);
-	printf("STR CPY [%s] PREFIX [%s] [len = %d]\n", new, str, size + 1);
-	i = ft_memcmp(new, str, size + 1);
-	free(new);
-	// printf("RETURN [%d]\n", i);
-	return (i);
-}
-
-t_node	*find_equal_return_ptr(t_lst *lst, char ch)
-{
-	int		i;
-	t_node	*node;
-
-	if (!lst)
-		return (NULL);
-	if (lst->size == 0)
-		return (NULL);
-	i = 1;
-	node = lst->head;
-	while (i <= lst->size)
-	{
-		if (node->c == ch)
-		{
-			// node = node->next;
-			return (node);
-		}
-		i++;
-		node = node->next;
-	}
-	return (NULL);
-}
-
-void export(t_ast_n *cmd, t_mini *mini, t_var_exe *var)
+void	exe_export(t_ast_n *cmd, t_mini *mini, t_var_exe *var)
 {
 	t_llst	*line;
 	int		i;
 	int		i_color;
 
+	line = mini->m_lst_env->head;
+	i = 1;
+	i_color = 0;
+	while (i <= mini->m_lst_env->size)
+	{
+		if (i_color > 3)
+			i_color = 0;
+		ft_putstr_fd(mini->color[i_color], STDOUT_FILENO);
+		ft_putlst_fd(line->lst, 1, STDOUT_FILENO);
+		ft_putstr_fd(RESET, STDOUT_FILENO);
+		i_color++;
+		line = line->next;
+		i++;
+	}
+	if (cmd->m_lst->next->type == PIPE)
+	{
+		close (mini->fd_std[0]);
+		close (mini->fd_std[1]);
+		free_memory(mini, var, 1);
+	}
+}
+
+char	*is_var_in_env(t_ast_n *cmd, t_mini *mini, t_llst *llst, char *new_var)
+{
+	t_llst	*env;
+	char	*prefix;
+	int		index;
+
+	env = mini->m_lst_env->head;
+	index = 0;
+	while (new_var && index < mini->m_lst_env->size)
+	{
+		prefix = NULL;
+		prefix = get_prfx(find_type_rtrn_ptr(env->lst, EQUAL_SING), env->lst);
+		if (prefix && !ft_strncmp(new_var, prefix, ft_strlen(prefix) + 1))
+		{
+			ft_add_mlstnode_back(mini->m_lst_env,
+				mlst_rmv_return_lnode(cmd->m_lst->matrix, llst));
+			ft_rmv_spcfc_lst_mtrx(mini->m_lst_env, env);
+			free(prefix);
+			free(new_var);
+			new_var = NULL;
+			break ;
+		}
+		free(prefix);
+		index++;
+		env = env->next;
+	}
+	return (new_var);
+}
+
+void	exe_export_var(t_ast_n *cmd, t_mini *mini)
+{
+	t_llst	*llst;
+	char	*new_var;
+
+	ft_remove_lst_front(cmd->m_lst->matrix);
+	llst = cmd->m_lst->matrix->head;
+	while (cmd->m_lst->matrix->size)
+	{
+		new_var = get_prfx(find_type_rtrn_ptr(llst->lst, EQUAL_SING),
+				llst->lst);
+		if (!new_var)
+		{
+			ft_remove_lst_front(cmd->m_lst->matrix);
+			if (cmd->m_lst->matrix->size == 0)
+				break ;
+			llst = cmd->m_lst->matrix->head;
+		}
+		new_var = is_var_in_env(cmd, mini, llst, new_var);
+		if (new_var && ft_strlen(new_var))
+		{
+			ft_add_mlstnode_back(mini->m_lst_env,
+				mlst_rmv_return_lnode(cmd->m_lst->matrix, llst));
+			free(new_var);
+		}
+		llst = cmd->m_lst->matrix->head;
+	}
+}
+
+void	export(t_ast_n *cmd, t_mini *mini, t_var_exe *var)
+{
 	if (cmd->m_lst->prev->type == AND_OP && g_status_child != 0)
 		return ;
 	if (cmd->m_lst->prev->type == OR_OP && g_status_child == 0)
@@ -67,91 +109,8 @@ void export(t_ast_n *cmd, t_mini *mini, t_var_exe *var)
 	ft_valid_command_builtin(cmd);
 	g_status_child = 0;
 	if (cmd->m_lst->matrix->size == 1)
-	{
-		line = mini->m_lst_env->head;
-		i = 1;
-		i_color = 0;
-		while (i <= mini->m_lst_env->size)
-		{
-			if (i_color > 3)
-				i_color = 0;
-			ft_putstr_fd(mini->color[i_color], STDOUT_FILENO);
-			ft_putlst_fd(line->lst, 1, STDOUT_FILENO);
-			ft_putstr_fd(RESET, STDOUT_FILENO);
-			i_color++;
-			line = line->next;
-			i++;
-		}
-		if (cmd->m_lst->next->type == PIPE)
-		{
-			close (mini->fd_std[0]);
-			close (mini->fd_std[1]);
-			free_memory(mini, var, 1);
-		}
-	}
+		exe_export(cmd, mini, var);
 	else
-	{
-		t_llst	*llst;
-		t_llst	*env;
-		char	*prefix;
-		char	*new_var;
-		int		index;
-	
-		ft_remove_lst_front(cmd->m_lst->matrix);
-		llst = cmd->m_lst->matrix->head;
-		while (cmd->m_lst->matrix->size)
-		{
-			env = mini->m_lst_env->head;
-			index = 0;
-			new_var = get_prfx(find_equal_return_ptr(llst->lst, '='), llst->lst);
-			if (!new_var)
-			{
-				ft_remove_lst_front(cmd->m_lst->matrix);
-				if (!cmd->m_lst->matrix->size)
-					break ;
-				llst = cmd->m_lst->matrix->head;
-			}
-			while(index < mini->m_lst_env->size)
-			{
-				prefix = NULL;
-				prefix = get_prfx(find_equal_return_ptr(env->lst, '='), env->lst);
-				if (prefix)
-				{
-
-					if (!ft_strncmp(new_var, prefix, ft_strlen(prefix) + 1))
-					{
-						ft_add_mlstnode_back(mini->m_lst_env, mlst_rmv_return_lnode(cmd->m_lst->matrix, llst));
-						ft_rmv_spcfc_lst_mtrx(mini->m_lst_env, env);
-						free(prefix);
-						free(new_var);
-						new_var = NULL;
-						break; ;
-					}
-				}
-				free(prefix);
-				index++;
-				env = env->next;
-			}
-			if (new_var && ft_strlen(new_var))
-			{
-				ft_add_mlstnode_back(mini->m_lst_env, mlst_rmv_return_lnode(cmd->m_lst->matrix, llst));
-				free(new_var);
-			}
-			llst = cmd->m_lst->matrix->head;
-		}
-	}
+		exe_export_var(cmd, mini);
 	finished_builtin(cmd, mini, var);
 }
-
-/* static void ft_manager_fd(t_ast_n *cmd, t_mini *mini, t_var_exe *var)
-{
-	if (cmd->m_lst->next->type == PIPE)
-	{
-		dup2(var->tube[1], STDOUT_FILENO);
-		close(var->tube[0]);
-		close(var->tube[1]);
-	}
-	if (ft_redirect_manager(cmd->m_lst->matrix) < 0)
-		free_memory(mini, var, 1);
-}
- */
